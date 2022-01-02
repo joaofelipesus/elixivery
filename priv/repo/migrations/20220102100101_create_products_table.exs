b6d623dfa6a1e7ebb5ed24ac6dc596f9b6282305
joaofelipe <joaofelipe1294@gmail.com>
2022-01-02 07:04:17 -0300

    feat: add products table.

---
+++ priv/repo/migrations/20220102100101_create_products_table.exs
@@ -0,0 +1,14 @@
+defmodule Elixivery.Repo.Migrations.CreateProductsTable do
+  use Ecto.Migration
+
+  def change do
+    create table("products") do
+      add :name, :string
+      add :price, :decimal
+      add :kind, :product_kind
+      add :restaurant_id, references(:restaurants, type: :binary_id)
+      timestamps()
+    end
+    create unique_index(:products, [:name])
+  end
+end
