54fcacd263229730516c78af8a14827965902de7
joaofelipe <joaofelipe1294@gmail.com>
2022-01-02 07:07:37 -0300

    fix: product kind possible values.

--- priv/repo/migrations/20220102095810_create_product_kind_type.exs
+++ priv/repo/migrations/20220102095810_create_product_kind_type.exs
@@ -2,7 +2,7 @@ defmodule Elixivery.Repo.Migrations.CreateProductKindType do
   use Ecto.Migration
 
   def change do
-    up_query = "CREATE TYPE product_kind AS ENUM('food', 'good', 'deasert')"
+    up_query = "CREATE TYPE product_kind AS ENUM('food', 'drink', 'deasert')"
     down_query = "DROP TYPE product_kind"
     execute(up_query, down_query)
   end
