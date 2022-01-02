271cfa444a512f04ed59436dff8f6e0bd204387b
joaofelipe <joaofelipe1294@gmail.com>
2022-01-02 07:00:40 -0300

    feat: add product_kind kind value.

---
+++ priv/repo/migrations/20220102095810_create_product_kind_type.exs
@@ -0,0 +1,9 @@
+defmodule Elixivery.Repo.Migrations.CreateProductKindType do
+  use Ecto.Migration
+
+  def change do
+    up_query = "CREATE TYPE product_kind AS ENUM('food', 'good', 'deasert')"
+    down_query = "DROP TYPE product_kind"
+    execute(up_query, down_query)
+  end
+end
